12a11390377d369070660b485a81a479a97ab639
samanyadaengdej <samannya2545@gmail.com>
2024-02-06 00:04:59 +0700

    edit

---
+++ Test 1/server/app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+let todoList = [];
+
+app.get('/api/todos', (req, res) => {
+  res.json(todoList);
+});
+
+app.post('/api/todos', (req, res) => {
+  const { task } = req.body;
+
+  if (!task) {
+    return res.status(400).json({ error: 'Task is required' });
+  }
+
+  const newTodo = {
+    id: uuidv4(),
+    task,
+    completed: false,
+  };
+
+  todoList.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+app.put('/api/todos/:id', (req, res) => {
+  const todoId = req.params.id;
+  const { completed } = req.body;
+
+  const todoIndex = todoList.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex === -1) {
+    return res.status(404).json({ error: 'To-Do not found' });
+  }
+
+  todoList[todoIndex].completed = completed;
+  res.json(todoList[todoIndex]); // Respond with the modified to-do item
+});
+
+
+app.delete('/api/todos/:id', (req, res) => {
+  const todoId = req.params.id;
+  todoList = todoList.filter((todo) => todo.id !== todoId);
+  res.json({ message: 'To-Do deleted successfully' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
